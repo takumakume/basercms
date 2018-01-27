270760a6b5f217d1c21bfa17553f53dca26cc916
Sunao Kiyosue <kiyo@itm.co.jp>
2018-01-28 08:23:41 +0900

    メールプラグインの基本設定画面の保存ボタンへスタイル適応

--- app/webroot/theme/ownd_river/MailConfigs/admin/form.php
+++ app/webroot/theme/ownd_river/MailConfigs/admin/form.php
@@ -74,6 +74,6 @@
 		<?php echo $this->BcForm->dispatchAfterForm() ?>
 	</table>
 </div>
-<div class="submit"><?php echo $this->BcForm->submit('保存', array('div' => false, 'class' => 'button')) ?></div>
+<div class="submit"><?php echo $this->BcForm->submit('保存', array('div' => false, 'class' => 'button bca-btn', 'data-bca-btn-type' => 'save')) ?></div>
 
 <?php echo $this->BcForm->end() ?>
