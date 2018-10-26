7c63503e08704e9469ee88419dcff33baf7bae61
Miho <miho.n110@gmail.com>
2018-10-26 10:22:37 +0900

    チェックボックスクラス変更（途中）

--- app/webroot/theme/ownd_river/Elements/admin/permissions/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/permissions/index_list.php
@@ -30,7 +30,7 @@
 	</div>
 	<?php if ($this->BcBaser->isAdminUser()): ?>
 		<div>
-			<?php echo $this->BcForm->checkbox('ListTool.checkall', array('title' => __d('baser', '一括選択'))) ?>
+			<?php echo $this->BcForm->checkbox('ListTool.checkall', array('title' => __d('baser', '一括選択'),'class'=>'bca-checkbox')) ?>
 			<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('publish' => __d('baser', '有効'), 'unpublish' => __d('baser', '無効'), 'del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
 			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn')) ?>
 		</div>
