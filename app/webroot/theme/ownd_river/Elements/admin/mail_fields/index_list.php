c5e7af372f037dd504c02ba6d0157d467b662a62
kato <kato@e-catchup.jp>
2018-12-02 21:43:05 +0900

    mail_fields/index 調整

--- app/webroot/theme/ownd_river/Elements/admin/mail_fields/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/mail_fields/index_list.php
@@ -14,6 +14,18 @@
  * [ADMIN] メールフィールド 一覧　テーブル
  */
 ?>
+<div class="bca-data-list__top">
+<!-- 一括処理 -->
+	<?php if ($this->BcBaser->isAdminUser()): ?>
+		<div>
+			<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('publish' => __d('baser', '有効'), 'unpublish' => __d('baser', '無効'), 'del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
+			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn', 'data-bca-btn-size' => 'lg')) ?>
+		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn','hidden' => 'hidden')) . __d('baser', '新規追加'), array('action' => 'add', $mailContent['MailContent']['id']),array('class'=>'bca-table-listup__a bca-btn', 'data-bca-btn-size' => 'lg', 'data-bca-btn-type' => 'add')) ?>
+		</div>
+	<?php endif ?>
+
+</div>
+
 
 
 <table cellpadding="0" cellspacing="0" class="list-table sort-table bca-table-listup" id="ListTable">
@@ -21,28 +33,23 @@
 		<tr>
 			<th class="list-tool bca-table-listup__thead-th">
 	<div>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn','hidden' => 'hidden')) . __d('baser', '新規追加'), array('action' => 'add', $mailContent['MailContent']['id']),array('class'=>' bca-table-listup__a bca-btn', 'data-bca-btn-type' => 'add')) ?>
+			<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
+			<label for="ListToolCheckall" data-bca-checkbox-size="sm" class="bca-checkbox-label"></label>
 		<?php if (!$sortmode): ?>
 			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_sort.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', '並び替え'), 'class' => 'btn')), array($mailContent['MailContent']['id'], 'sortmode' => 1)) ?>
 		<?php else: ?>
 			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_normal.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', 'ノーマル'), 'class' => 'btn')), array($mailContent['MailContent']['id'], 'sortmode' => 0)) ?>
 		<?php endif ?>
 	</div>
-	<?php if ($this->BcBaser->isAdminUser()): ?>
-		<div>
-			<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
-			<label for="ListToolCheckall" data-bca-checkbox-size="sm" class="bca-checkbox-label"></label>
-			<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('publish' => __d('baser', '有効'), 'unpublish' => __d('baser', '無効'), 'del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
-			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn')) ?>
-		</div>
-	<?php endif ?>
-</th>
+
+			</th>
 <th class="bca-table-listup__thead-th">NO</th>
 <th class="bca-table-listup__thead-th">フィールド名<br />項目名</th>
 <th class="bca-table-listup__thead-th">タイプ</th>
 <th class="bca-table-listup__thead-th">グループ名</th>
 <th class="bca-table-listup__thead-th">必須</th>
 <th class="bca-table-listup__thead-th">登録日<br />更新日</th>
+<th class="bca-table-listup__thead-th"><?php echo __d('baser', 'アクション') ?></th>
 </tr>
 </thead>
 <tbody>
--- app/webroot/theme/ownd_river/Elements/admin/mail_fields/index_row.php
+++ app/webroot/theme/ownd_river/Elements/admin/mail_fields/index_row.php
@@ -22,30 +22,33 @@ if (!$data['MailField']['use_field']) {
 
 
 <tr id="Row<?php echo $count + 1 ?>" <?php echo $class; ?>>
-	<td style="width:25%" class="row-tools bca-table-listup__tbody-td">
+	<td class="row-tools bca-table-listup__tbody-td">
+		<?php if ($this->BcBaser->isAdminUser()): ?>
+		<?php echo $this->BcForm->input('ListTool.batch_targets.' . $data['MailField']['id'], ['type' => 'checkbox', 'class' => 'batch-targets', 'value' => $data['MailField']['id']]) ?>
+		<?php endif ?>	
 		<?php if ($sortmode): ?>
 			<span class="sort-handle"><?php $this->BcBaser->img('admin/sort.png', array('alt' => __d('baser', '並び替え'))) ?></span>
 			<?php echo $this->BcForm->hidden('Sort.id' . $data['MailField']['id'], array('class' => 'id', 'value' => $data['MailField']['id'])) ?>
 		<?php endif ?>
-		<?php if ($this->BcBaser->isAdminUser()): ?>
-			<?php echo $this->BcForm->input('ListTool.batch_targets.' . $data['MailField']['id'], ['type' => 'checkbox', 'class' => 'batch-targets', 'value' => $data['MailField']['id']]) ?>
-		<?php endif ?>	
+	</td>
+	<td class="bca-table-listup__tbody-td"><?php echo $data['MailField']['no'] ?></td>
+	<td class="bca-table-listup__tbody-td">
+		<?php $this->BcBaser->link($data['MailField']['field_name'], array('action' => 'edit', $mailContent['MailContent']['id'], $data['MailField']['id'])) ?><br />
+		<?php echo $data['MailField']['name'] ?>
+	</td>
+	<td class="bca-table-listup__tbody-td"><?php echo $this->BcText->listValue('MailField.type', $data['MailField']['type']) ?></td>
+	<td class="bca-table-listup__tbody-td"><?php echo $data['MailField']['group_field'] ?></td>
+	<td class="bca-table-listup__tbody-td"><?php echo $this->BcText->booleanMark($data['MailField']['not_empty']) ?></td>
+	<td class="bca-table-listup__tbody-td">
+		<?php echo $this->BcTime->format('Y-m-d', $data['MailField']['created']) ?><br />
+		<?php echo $this->BcTime->format('Y-m-d', $data['MailField']['modified']) ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
 		<?php $this->BcBaser->link('', array('action' => 'ajax_unpublish', $mailContent['MailContent']['id'], $data['MailField']['id']), array('title' => __d('baser', '非公開'), 'class' => 'btn-unpublish bca-btn-icon', 'data-bca-btn-type' => 'unpublish','data-bca-btn-size' => 'lg')) ?>
 		<?php $this->BcBaser->link('', array('action' => 'ajax_publish', $mailContent['MailContent']['id'], $data['MailField']['id']), array('title' => __d('baser', '公開'), 'class' => 'btn-publish bca-btn-icon', 'data-bca-btn-type' => 'publish','data-bca-btn-size' => 'lg')) ?>
 		<?php $this->BcBaser->link('', array('action' => 'edit', $mailContent['MailContent']['id'], $data['MailField']['id']), array('title' => __d('baser', '編集'), 'class' => ' bca-btn-icon', 'data-bca-btn-type' => 'edit','data-bca-btn-size' => 'lg')) ?>
 		<?php $this->BcBaser->link('', array('action' => 'ajax_copy', $mailContent['MailContent']['id'], $data['MailField']['id']), array('title' => __d('baser', 'コピー'), 'class' => 'btn-copy bca-icon--copy bca-btn-icon', 'data-bca-btn-type' => 'copy','data-bca-btn-size' => 'lg')) ?>
 		<?php $this->BcBaser->link('', array('action' => 'ajax_delete', $mailContent['MailContent']['id'], $data['MailField']['id']), array('title' => __d('baser', '削除'), 'class' => 'btn-delete bca-btn-icon', 'data-bca-btn-type' => 'delete','data-bca-btn-size' => 'lg')) ?>
 	</td>
-	<td class="bca-table-listup__tbody-td" style="width:5%"><?php echo $data['MailField']['no'] ?></td>
-	<td class="bca-table-listup__tbody-td" style="width:25%">
-		<?php $this->BcBaser->link($data['MailField']['field_name'], array('action' => 'edit', $mailContent['MailContent']['id'], $data['MailField']['id'])) ?><br />
-		<?php echo $data['MailField']['name'] ?>
-	</td>
-	<td class="bca-table-listup__tbody-td" style="width:15%"><?php echo $this->BcText->listValue('MailField.type', $data['MailField']['type']) ?></td>
-	<td class="bca-table-listup__tbody-td" style="width:10%"><?php echo $data['MailField']['group_field'] ?></td>
-	<td class="bca-table-listup__tbody-td" style="width:8%;text-align:center"><?php echo $this->BcText->booleanMark($data['MailField']['not_empty']) ?></td>
-	<td class="bca-table-listup__tbody-td" style="width:12%;white-space:nowrap">
-		<?php echo $this->BcTime->format('Y-m-d', $data['MailField']['created']) ?><br />
-		<?php echo $this->BcTime->format('Y-m-d', $data['MailField']['modified']) ?>
-	</td>
+
 </tr>
