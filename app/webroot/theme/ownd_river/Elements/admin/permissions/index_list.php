74edb5178059e67efeb2639584950aa63955dcbf
kato <kato@e-catchup.jp>
2018-12-02 20:02:35 +0900

    /permissions/index/2 作成（アイコン旧）

--- app/webroot/theme/ownd_river/Elements/admin/permissions/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/permissions/index_list.php
@@ -14,36 +14,35 @@
  * [ADMIN] アクセス制限設定一覧
  */
 ?>
-
-
-<table cellpadding="0" cellspacing="0" class="list-table sort-table" id="ListTable">
-	<thead>
-		<tr>
-			<th class="list-tool">
-	<div>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn')), array('action' => 'add', $this->request->params['pass'][0])) ?>
-		<?php if (!$sortmode): ?>
-			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_sort.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', '並び替え'), 'class' => 'btn')), array('sortmode' => 1, $this->request->params['pass'][0])) ?>
-		<?php else: ?>
-			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_normal.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', 'ノーマル'), 'class' => 'btn')), array('sortmode' => 0, $this->request->params['pass'][0])) ?>
-		<?php endif ?>
-	</div>
+<div class="bca-data-list__top">
+<!-- 一括処理 -->
+	<div class="bca-action-table-listup">
 	<?php if ($this->BcBaser->isAdminUser()): ?>
-		<div>
-<<<<<<< HEAD
-			<?php echo $this->BcForm->checkbox('ListTool.checkall', array('title' => __d('baser', '一括選択'),'class'=>'bca-checkbox')) ?>
-=======
-			<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
->>>>>>> 60b0699c07c1d44400a067674ab0dcb2a5917dd7
-			<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('publish' => __d('baser', '有効'), 'unpublish' => __d('baser', '無効'), 'del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
-			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn')) ?>
-		</div>
+		<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('publish' => __d('baser', '有効'), 'unpublish' => __d('baser', '無効'), 'del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'), 'data-bca-select-size' =>'lg')) ?>
+		<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn', 'data-bca-btn-size' => 'lg')) ?>
+	<?php endif ?>
+	<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn bca-table-listup__img', 'hidden' => 'hidden')) . __d('baser', '新規追加'), array('action' => 'add', $this->request->params['pass'][0]),array('class'=>'bca-table-listup__a bca-btn', 'data-bca-btn-size' => 'lg', 'data-bca-btn-type' => 'add')) ?>
+	</div>
+</div>
+
+
+<table cellpadding="0" cellspacing="0" class="list-table sort-table bca-table-listup" id="ListTable">
+	<thead class="bca-table-listup__thead">
+		<tr>
+<th class="list-tool bca-table-listup__thead-th  bca-table-listup__thead-th--select"><?php // 一括選択 ?>
+	<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
+	<label for="ListToolCheckall" data-bca-checkbox-size="sm" class="bca-checkbox-label"></label>
+	<?php if (!$sortmode): ?>
+		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_sort.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', '並び替え'), 'class' => 'btn　bca-btn')), array('sortmode' => 1, $this->request->params['pass'][0])) ?>
+	<?php else: ?>
+		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_normal.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', 'ノーマル'), 'class' => 'btn　bca-btn')), array('sortmode' => 0, $this->request->params['pass'][0])) ?>
 	<?php endif ?>
 </th>
-<th>NO</th>
-<th>ルール名<br />URL設定</th>
-<th>アクセス</th>
-<th>登録日<br />更新日</th>
+<th class="bca-table-listup__thead-th">NO</th>
+<th class="bca-table-listup__thead-th">ルール名<br />URL設定</th>
+<th class="bca-table-listup__thead-th">アクセス</th>
+<th class="bca-table-listup__thead-th">登録日<br />更新日</th>
+<th class="bca-table-listup__thead-th">アクション</th>
 </tr>
 </thead>
 <tbody>
--- app/webroot/theme/ownd_river/Elements/admin/permissions/index_row.php
+++ app/webroot/theme/ownd_river/Elements/admin/permissions/index_row.php
@@ -22,30 +22,32 @@
 	<?php $class = ' class="publish sortable"'; ?>
 <?php endif; ?>
 <tr<?php echo $class; ?>>
-	<td style="width:15%" class="row-tools">
+	<td class="row-tools bca-table-listup__tbody-td ">
+		<?php if ($this->BcBaser->isAdminUser()): ?>
+			<?php echo $this->BcForm->input('ListTool.batch_targets.' . $data['Permission']['id'], ['type' => 'checkbox', 'class' => 'batch-targets', 'value' => $data['Permission']['id']]) ?>
+		<?php endif ?>
 		<?php if ($sortmode): ?>
 			<span class="sort-handle"><?php $this->BcBaser->img('admin/sort.png', array('alt' => __d('baser', '並び替え'))) ?></span>
 			<?php echo $this->BcForm->input('Sort.id' . $data['Permission']['id'], array('type' => 'hidden', 'class' => 'id', 'value' => $data['Permission']['id'])) ?>
 		<?php endif ?>
-		<?php if ($this->BcBaser->isAdminUser()): ?>
-			<?php echo $this->BcForm->input('ListTool.batch_targets.' . $data['Permission']['id'], ['type' => 'checkbox', 'class' => 'batch-targets', 'value' => $data['Permission']['id']]) ?>
-		<?php endif ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_unpublish.png', array('alt' => __d('baser', '無効'), 'class' => 'btn')), array('action' => 'ajax_unpublish', $data['Permission']['id']), array('title' => __d('baser', '非公開'), 'class' => 'btn-unpublish')) ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_publish.png', array('alt' => __d('baser', '有効'), 'class' => 'btn')), array('action' => 'ajax_publish', $data['Permission']['id']), array('title' => __d('baser', '公開'), 'class' => 'btn-publish')) ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_edit.png', array('alt' => __d('baser', '編集'), 'class' => 'btn')), array('action' => 'edit', $this->request->params['pass'][0], $data['Permission']['id']), array('title' => __d('baser', '編集'))) ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_copy.png', array('alt' => __d('baser', 'コピー'), 'class' => 'btn')), array('action' => 'ajax_copy', $this->request->params['pass'][0], $data['Permission']['id']), array('title' => __d('baser', 'コピー'), 'class' => 'btn-copy')) ?>
-		<?php if ($data['Permission']['name'] != 'admins'): ?>
-			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_delete.png', array('alt' => __d('baser', '削除'), 'class' => 'btn')), array('action' => 'ajax_delete', $data['Permission']['id']), array('title' => __d('baser', '削除'), 'class' => 'btn-delete')) ?>
-		<?php endif ?>
 	</td>
-	<td style="width:10%"><?php echo $data['Permission']['no']; ?></td>
-	<td style="width:55%">
+	<td class="bca-table-listup__tbody-td"><?php echo $data['Permission']['no']; ?></td>
+	<td class="bca-table-listup__tbody-td">
 		<?php $this->BcBaser->link($data['Permission']['name'], array('action' => 'edit', $this->request->params['pass'][0], $data['Permission']['id'])); ?><br />
 		<?php echo $data['Permission']['url']; ?>
 	</td>
-	<td style="width:10%" class="bc-align-center"><?php echo $this->BcText->arrayValue($data['Permission']['auth'], array(0 => '×', 1 => '〇')) ?></td>
-	<td style="width:10%">
+	<td class="bca-table-listup__tbody-td"><?php echo $this->BcText->arrayValue($data['Permission']['auth'], array(0 => '×', 1 => '〇')) ?></td>
+	<td class="bca-table-listup__tbody-td">
 		<?php echo $this->BcTime->format('Y-m-d', $data['Permission']['created']); ?><br />
 		<?php echo $this->BcTime->format('Y-m-d', $data['Permission']['modified']); ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
+		<?php $this->BcBaser->link('', array('action' => 'ajax_unpublish', $data['Permission']['id']), array('title' => __d('baser', '非公開'), 'class' => 'btn-unpublish bca-btn-icon', 'data-bca-btn-type' => 'unpublish','data-bca-btn-size' => 'lg')) ?>
+		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_publish.png', array('alt' => __d('baser', '有効'), 'class' => 'btn')), array('action' => 'ajax_publish', $data['Permission']['id']), array('title' => __d('baser', '公開'), 'class' => 'btn-publish')) ?>
+		<?php $this->BcBaser->link('', array('action' => 'edit', $this->request->params['pass'][0], $data['Permission']['id']), array('title' => __d('baser', '編集'), 'class' => ' bca-btn-icon', 'data-bca-btn-type' => 'edit','data-bca-btn-size' => 'lg')) ?>
+		<?php $this->BcBaser->link('', array('action' => 'ajax_copy', $this->request->params['pass'][0], $data['Permission']['id']), array('title' => __d('baser', 'コピー'), 'class' => 'btn-copy bca-icon--copy bca-btn-icon', 'data-bca-btn-type' => 'copy','data-bca-btn-size' => 'lg')) ?>
+		<?php if ($data['Permission']['name'] != 'admins'): ?>
+			<?php $this->BcBaser->link('', array('action' => 'ajax_delete', $data['Permission']['id']), array('title' => __d('baser', '削除'), 'class' => 'btn-delete bca-btn-icon', 'data-bca-btn-type' => 'delete','data-bca-btn-size' => 'lg')) ?>
+		<?php endif ?>
 	</td>
 </tr>
