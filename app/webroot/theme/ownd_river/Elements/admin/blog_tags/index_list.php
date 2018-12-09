ecda7f8da01ee4658a940de19d61013d77148687
kato <kato@e-catchup.jp>
2018-12-09 11:02:47 +0900

    blog/blog_tags/ 一括選択ボタン　スタイル当てる

--- app/webroot/theme/ownd_river/Elements/admin/blog_tags/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/blog_tags/index_list.php
@@ -14,34 +14,30 @@
  * [ADMIN] ブログタグ一覧　テーブル
  */
 ?>
-
 <div class="bca-data-list__top">
 <!-- 一括処理 -->
 	<?php if ($this->BcBaser->isAdminUser()): ?>
 		<div class="bca-action-table-listup">
-			<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
+			<?php echo $this->BcForm->input('ListTool.batch', ['type' => 'select', 'options' => array('del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'), 'data-bca-select-size' =>'lg']) ?>
 			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn', 'data-bca-btn-size' => 'lg')) ?>
 		</div>
 	<?php endif ?>
-
-	<div class="bca-data-list__top">
-		<!-- pagination -->
-		 <div class="bca-data-list__sub">
-			<?php $this->BcBaser->element('pagination') ?>
-		</div>
-	</div>
+  <div class="bca-data-list__sub">
+    <!-- list-num -->
+    <?php $this->BcBaser->element('list_num') ?>
+    <!-- pagination -->
+    <?php $this->BcBaser->element('pagination') ?>
+  </div>
 </div>
+
+
 
 <!-- list -->
 <table cellpadding="0" cellspacing="0" class="list-table bca-table-listup" id="ListTable">
 	<thead class="bca-table-listup__thead">
 		<tr>
-			<th class="bca-table-listup__thead-th">
-			<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
-			<label for="ListToolCheckall" data-bca-checkbox-size="sm" class="bca-checkbox-label"></label>
-			</th>
-			<th style="width:160px" class="list-tool bca-table-listup__thead-th">
-				<?php echo __d('baser', 'アクション') ?>
+      <th class="list-tool bca-table-listup__thead-th bca-table-listup__thead-th--select"><?php // 一括選択 ?>
+        <?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'label' => __d('baser', '一括選択')]) ?>
 			</th>
 			<th class="bca-table-listup__thead-th">
 				<?php 
@@ -73,6 +69,9 @@
 					array('escape' => false, 'class' => 'btn-direction bca-table-listup__a')
 				); 
 				?>
+			</th>
+			<th class="list-tool bca-table-listup__thead-th">
+				<?php echo __d('baser', 'アクション') ?>
 			</th>
 		</tr>
 </thead>
