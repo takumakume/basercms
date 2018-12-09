e1355e07675bb6415cd88f3cb6243bb7e7c6a0cc
kato <kato@e-catchup.jp>
2018-12-09 17:53:48 +0900

    /widget_areas/index 新規追加ボタン　チェックボックス　調整

--- app/webroot/theme/ownd_river/Elements/admin/widget_areas/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/widget_areas/index_list.php
@@ -20,7 +20,6 @@
 	<div class="bca-action-table-listup">
 		<?php echo $this->BcForm->input('ListTool.batch', ['type' => 'select', 'options' => array('del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'), 'data-bca-select-size' =>'lg']) ?>
 		<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn', 'data-bca-btn-size' => 'lg')) ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn bca-table-listup__img', 'hidden' => 'hidden')) . __d('baser', '新規追加'), array('action' => 'add'), array('class'=>'bca-table-listup__a bca-btn', 'data-bca-btn-size' => 'lg', 'data-bca-btn-type' => 'add')) ?>
 	</div>
 <?php endif ?>
   <div class="bca-data-list__sub">
@@ -36,8 +35,7 @@
 <thead class="bca-table-listup__thead">
 	<tr class="">
 		<th class="list-tool bca-table-listup__thead-th  bca-table-listup__thead-th--select">
-			<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
-			<label for="ListToolCheckall" data-bca-checkbox-size="sm" class="bca-checkbox-label"></label>
+			<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'label' => __d('baser', '一括選択')]) ?>
 		</th>
 		<th class="bca-table-listup__thead-th">NO</th>
 		<th class="bca-table-listup__thead-th">ウィジェットエリア名</th>
