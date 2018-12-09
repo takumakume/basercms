07f3e9b4f66ace8802715dd6634f33b3a384ad06
Miho <miho.n110@gmail.com>
2018-12-09 17:34:01 +0900

    アップロードファイル一覧 （uploader.css以外）

--- app/webroot/theme/ownd_river/Elements/admin/uploader_files/index_panel.php
+++ app/webroot/theme/ownd_river/Elements/admin/uploader_files/index_panel.php
@@ -23,32 +23,34 @@ $this->BcBaser->js('admin/vendors/jquery.upload-1.0.0.min');
 
 <div id="DivPanelList">
 
-<?php $this->BcBaser->element('pagination') ?>
-
-<!-- form -->
-<?php if(!$installMessage): ?>
-<div id="UploaderForm" class="clearfix">
-	<div>
-		<?php echo $this->BcForm->label('UploaderFile.uploader_category_id', __d('baser', 'アップロード')) ?>&nbsp;
-	<?php if($uploaderCategories): ?>
-		<?php echo $this->BcForm->input('UploaderFile.uploader_category_id', array('type' => 'select', 'options' => $uploaderCategories, 'empty' => __d('baser', 'カテゴリ指定なし'), 'id' => 'UploaderFileUploaderCategoryId'.$listId)) ?>&nbsp;
-	<?php endif ?>
-		<span id="SpanUploadFile<?php echo $listId ?>">
-			<?php echo $this->BcForm->input('UploaderFile.file', array('type' => 'file', 'id'=>'UploaderFileFile'.$listId, 'class' => 'uploader-file-file')) ?>
-		</span>
+<div class="bca-list-head">
+	<!-- form -->
+	<?php if(!$installMessage): ?>
+	<div id="UploaderForm" class="clearfix">
+		<div>
+			<?php echo $this->BcForm->label('UploaderFile.uploader_category_id', __d('baser', 'アップロード')) ?>&nbsp;
+		<?php if($uploaderCategories): ?>
+			<?php echo $this->BcForm->input('UploaderFile.uploader_category_id', array('type' => 'select', 'options' => $uploaderCategories, 'empty' => __d('baser', 'カテゴリ指定なし'), 'id' => 'UploaderFileUploaderCategoryId'.$listId)) ?>&nbsp;
+		<?php endif ?>
+			<span id="SpanUploadFile<?php echo $listId ?>">
+				<?php echo $this->BcForm->input('UploaderFile.file', array('type' => 'file', 'id'=>'UploaderFileFile'.$listId, 'class' => 'uploader-file-file')) ?>
+			</span>
+		</div>
 	</div>
-</div>
-<?php else: ?>
-<p style="color:#C00;font-weight:bold"><?php echo $installMessage ?></p>
-<?php endif ?>
+	<?php else: ?>
+	<p style="color:#C00;font-weight:bold"><?php echo $installMessage ?></p>
+	<?php endif ?>
 
-<div class="file-list-body clearfix corner5">
+	<?php $this->BcBaser->element('pagination') ?>
+</div>
+
+<div class="file-list-body clearfix corner5 bca-file-list">
 <?php if ($files): ?>
 	<?php foreach ($files as $file): ?>
 		<?php $this->BcBaser->element('uploader_files/index_box', array('file' => $file, 'users' => $users)) ?>
 	<?php endforeach ?>
 <?php else: ?>
-<p class="no-data">ファイルが存在しません</p>
+<p class="no-data bca-file-list__no-data">ファイルが存在しません</p>
 <?php endif ?>
 </div>
 
