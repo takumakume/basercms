d20aff53bbfc68e8738987e04caf4b2cac758bb1
Miho <miho.n110@gmail.com>
2018-12-11 23:55:14 +0900

    /admin/users/ ユーザ一覧画面完成、ログインアイコン修正

--- app/webroot/theme/ownd_river/Elements/admin/users/index_row.php
+++ app/webroot/theme/ownd_river/Elements/admin/users/index_row.php
@@ -24,24 +24,10 @@
 	<td class="bca-table-listup__tbody-td"><?php echo $this->BcTime->format('Y-m-d', $data['User']['created']) ?><br />
 		<?php echo $this->BcTime->format('Y-m-d', $data['User']['modified']) ?></td>
 	<td class="row-tools bca-table-listup__tbody-td">
-		<?php $this->BcBaser->link(
-			'', 
-			array(
-				'action' => 'edit', $data['User']['id']
-			), 
-			array(
-				'title' => __d('baser', '編集'), 'class' => ' bca-btn-icon', 'data-bca-btn-type' => 'edit','data-bca-btn-size' => 'lg'
-			)
-		) ?>
-
-		<?php $this->BcBaser->link(
-			'', 
-			array(
-				'action' => 'ajax_delete', $data['User']['id']), array('title' => __d('baser', '削除'), 'class' => 'btn-delete bca-btn-icon', 'data-bca-btn-type' => 'delete','data-bca-btn-size' => 'lg'
-			)
-		) ?>
+		<?php $this->BcBaser->link('', array('action' => 'edit', $data['User']['id']), array('title' => __d('baser', '編集'), 'class' => ' bca-btn-icon', 'data-bca-btn-type' => 'edit','data-bca-btn-size' => 'lg')) ?>
+		<?php $this->BcBaser->link('', array('action' => 'ajax_delete', $data['User']['id']), array('title' => __d('baser', '削除'), 'class' => 'btn-delete bca-btn-icon', 'data-bca-btn-type' => 'delete','data-bca-btn-size' => 'lg')) ?>
 		<?php if (!$this->BcBaser->isAdminUser($data['User']['user_group_id'])): ?>
-			<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_login.png', array('alt' => __d('baser', 'ログイン'), 'class' => 'btn')), array('action' => 'ajax_agent_login', $data['User']['id']), array('title' => __d('baser', 'ログイン'), 'class' => 'btn-login')) ?>
+			<?php $this->BcBaser->link('', array('action' => 'ajax_agent_login', $data['User']['id']), array('title' => __d('baser', 'ログイン'), 'class' => 'btn-login bca-btn-icon', 'data-bca-btn-type' => 'switch','data-bca-btn-size' => 'lg')) ?>
 		<?php endif ?>
 	</td>
 </tr>
