b022dc1725e9969980f8f131f55cea18e7ee8aa3
kato <kato@e-catchup.jp>
2018-12-16 20:06:18 +0900

    /Elements/admin/uploader_categories/index_list.php /Elements/admin/uploader_categories/index_row.php /ownd_river/UploaderCategories/admin/index.php 組込

--- app/webroot/theme/ownd_river/Elements/admin/uploader_categories/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/uploader_categories/index_list.php
@@ -11,9 +11,18 @@
  */
 ?>
 
-<div class="bca-list-head">
-	<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn', 'hidden' => 'hidden')) . __d('baser', '新規追加'), array('action' => 'add'), array('class' => 'bca-btn', 'data-bca-btn-type' => 'add')) ?>
-	<?php $this->BcBaser->element('pagination') ?>
+<div class="bca-data-list__top">
+<!-- 一括処理 -->
+	<?php if ($this->BcBaser->isAdminUser()): ?>
+		<div class="bca-action-table-listup">
+			<?php echo $this->BcForm->input('ListTool.batch', ['type' => 'select', 'options' => array('del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'), 'data-bca-select-size' =>'lg']) ?>
+			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn', 'data-bca-btn-size' => 'lg')) ?>
+		</div>
+	<?php endif ?>
+  <div class="bca-data-list__sub">
+    <!-- pagination -->
+    <?php $this->BcBaser->element('pagination') ?>
+  </div>
 </div>
 
 <table cellpadding="0" cellspacing="0" class="list-table sort-table bca-table-listup" id="ListTable">
@@ -22,9 +31,7 @@
 			<th class="list-tool bca-table-listup__thead-th">
 				<?php if($this->BcBaser->isAdminUser()): ?>
 					<div>
-						<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
-						<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
-						<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn')) ?>
+						<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'label' => __d('baser', '一括選択')]) ?>
 					</div>
 				<?php endif ?>
 			</th>
