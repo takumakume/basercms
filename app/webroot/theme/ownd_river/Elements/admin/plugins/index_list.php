eae50b19ecb46bd30fac7f5ac6351bcaa7670a41
kato <kato@e-catchup.jp>
2018-12-16 19:30:07 +0900

    /Elements/admin/plugins/index_list.php　・　/Elements/admin/plugins/index_row.php　組込

--- app/webroot/theme/ownd_river/Elements/admin/plugins/index_list.php
+++ app/webroot/theme/ownd_river/Elements/admin/plugins/index_list.php
@@ -14,33 +14,37 @@
  * [ADMIN] プラグイン一覧　テーブル
  */
 ?>
+<div class="bca-data-list__top">
+<!-- 一括処理 -->
+	<?php if ($this->BcBaser->isAdminUser()): ?>
+		<div>
+			<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('publish' => __d('baser', '有効'), 'unpublish' => __d('baser', '無効'), 'del' => __d('baser', '削除')), 'empty' => __d('baser', '一括処理'))) ?>
+			<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn', 'data-bca-btn-size' => 'lg')) ?>
+		</div>
+	<?php endif ?>
+
+</div>
 
 
-<table cellpadding="0" cellspacing="0" class="list-table sort-table" id="ListTable">
-<thead>
+<table cellpadding="0" cellspacing="0" class="list-table sort-table bca-table-listup" id="ListTable">
+<thead class="bca-table-listup__thead">
 	<tr class="list-tool">
-		<th>
-			<div>
-				<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('width' => 69, 'height' => 18, 'alt' => __d('baser', '新規追加'), 'class' => 'btn')), array('action' => 'add')) ?>
-				<?php if (!$sortmode): ?>
-					<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_sort.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', '並び替え'), 'class' => 'btn')), array('sortmode' => 1)) ?>
-				<?php else: ?>
-					<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_normal.png', array('width' => 65, 'height' => 14, 'alt' => __d('baser', 'ノーマル'), 'class' => 'btn')), array('sortmode' => 0)) ?>
-				<?php endif ?>
-			</div>
+		<th class="list-tool bca-table-listup__thead-th bca-table-listup__thead-th--select"><?php // 一括選択 ?>
 			<?php if ($this->BcBaser->isAdminUser()): ?>
-			<div>
-				<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'title' => __d('baser', '一括選択')]) ?>
-				<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('del' => __d('baser', '一括無効')), 'empty' => __d('baser', '一括処理'))) ?>
-				<?php echo $this->BcForm->button(__d('baser', '適用'), array('id' => 'BtnApplyBatch', 'disabled' => 'disabled', 'class' => 'bca-btn')) ?>
-			</div>
+				<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'label' => __d('baser', '一括選択')]) ?>
+			<?php endif ?>
+			<?php if (!$sortmode): ?>
+				<?php $this->BcBaser->link('<i class="bca-btn-icon-text" data-bca-btn-type="draggable"></i>並び替え', array('sortmode' => 1)) ?>
+			<?php else: ?>
+				<?php $this->BcBaser->link('<i class="bca-btn-icon-text" data-bca-btn-type="draggable"></i>ノーマル', array('sortmode' => 0)) ?>
 			<?php endif ?>
 		</th>
-		<th>プラグイン名</th>
-		<th style="white-space: nowrap">バージョン</th>
-		<th>説明</th>
-		<th>開発者</th>
-		<th>登録日<br />更新日</th>
+		<th class="bca-table-listup__thead-th">プラグイン名</th>
+		<th class="bca-table-listup__thead-th" style="white-space: nowrap">バージョン</th>
+		<th class="bca-table-listup__thead-th">説明</th>
+		<th class="bca-table-listup__thead-th">開発者</th>
+		<th class="bca-table-listup__thead-th">登録日<br />更新日</th>
+		<th class="bca-table-listup__thead-th">アクション</th>
 	</tr>
 </thead>
 <tbody>
