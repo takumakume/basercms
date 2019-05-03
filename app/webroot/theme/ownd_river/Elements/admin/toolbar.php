9486567a2d95f2c6e1d7220a126dbcceeabde115
ryuring <ryuring55@gmail.com>
2019-05-03 12:43:06 +0900

    133 / 134 解消

--- app/webroot/theme/ownd_river/Elements/admin/toolbar.php
+++ app/webroot/theme/ownd_river/Elements/admin/toolbar.php
@@ -132,7 +132,7 @@ if (!empty($currentAuthPrefix['name']) && $currentPrefix != 'front') {
 				</li>
 				<?php if (!empty($user) && in_array('admin', $currentUserAuthPrefixes)): ?>
 					<li>
-						<?php $this->BcBaser->link(__d('baser', 'キャッシュクリア'), ['prefix' => 'admin', 'plugin'=> false, 'controller' => 'site_configs', 'action' => 'del_cache'], ['confirm' => __d('baser', 'キャッシュクリアします。いいですか？')]) ?>　
+						<?php $this->BcBaser->link(__d('baser', 'キャッシュクリア'), ['admin' => true, 'plugin'=> false, 'controller' => 'site_configs', 'action' => 'del_cache'], ['confirm' => __d('baser', 'キャッシュクリアします。いいですか？')]) ?>　
 					</li>
 				<?php endif ?>
 			</ul>
